c30dece25f1bc8f64cc867b6360f942430331a1b
JHallFS <mr.sl3go.jh@gmail.com>
2022-01-17 16:51:38 +0000

    whoops again

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,11 +1,19 @@
 import { Component } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route
+} from 'react-router-dom';
+
+import Nav from './components/Nav';
+
 import Home from './pages/home';
 
 class Portfolio extends Component {
 	render() {
 		return (
 			<Router>
+				<Nav />
 				<Routes>
 					<Route path="/" element={<Home/>} />
 				</Routes>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,10 @@
 import { Component } from 'react';
-import Nav from '../../components/Nav';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<>
-				<Nav />
+				<div>"Home" div</div>
 			</>
 		);
 	}
